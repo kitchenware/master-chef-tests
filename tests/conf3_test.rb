6e39dffa143aa2de212da9363df67bbd43973551
Bertrand Paquet <bpaquet@octo.com>
2012-05-05 15:59:12 +0200

    Add tests for timezone

--- tests/conf3_test.rb
+++ tests/conf3_test.rb
@@ -6,10 +6,12 @@ class TestConf3 < Test::Unit::TestCase
 
   def test_conf3
     @vm.upload_json "conf3.json"
-    @vm.run_chef
+    # @vm.run_chef
     @http.get 80, "/redmine"
     @http.assert_last_response_code 200
     @http.assert_last_response_body_regex /Redmine/
+    date = @vm.capture("date -u && date").split("\n").map{|x| x =~ /^.*(\d\d:\d\d:\d\d).*$/; $1}
+    assert_not_equal date[0], date[1]
   end
 
 end
