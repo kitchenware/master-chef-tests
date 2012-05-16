fff6598f6dc42a78737560b90ebeea49df432359
Mikael Robert <mikaelrob@gmail.com>
2012-05-16 17:08:36 +0200

    conf 5 test on sonar + nexus also test tomcat/nginx/mysql
    Signed-off-by: Mikael Robert <mikaelrob@gmail.com>

---
+++ tests/conf5_test.rb
@@ -0,0 +1,20 @@
+require File.join(File.dirname(__FILE__), '..', 'helper.rb')
+
+class TestConf5 < Test::Unit::TestCase
+
+  include VmTestHelper
+
+  def test_conf5
+    @vm.upload_json "conf5.json"
+    @vm.run_chef
+    @http.get 80, "/sonar/sessions/new?return_to=%2Fsonar%2F"
+    @http.assert_last_response_code 200
+    @http.assert_last_response_body_regex /Login/
+
+
+    @http.get 80, "/nexus/index.html#welcome"
+    @http.assert_last_response_code 200
+    @http.assert_last_response_body_regex /Sonatype Nexus/
+  end
+
+end
