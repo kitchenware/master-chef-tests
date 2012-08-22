8b06b1095a9610b83e350c1ef72e49867c4612f7
Bertrand Paquet <bpaquet@octo.com>
2012-08-22 14:11:02 +0200

    Add statsd on graphite nodes

--- tests/conf6_test.rb
+++ tests/conf6_test.rb
@@ -1,4 +1,5 @@
 require File.join(File.dirname(__FILE__), '..', 'helper.rb')
+require 'socket'
 
 class TestConf6 < Test::Unit::TestCase
 
@@ -9,7 +10,7 @@ class TestConf6 < Test::Unit::TestCase
     @vm.upload_json "conf6.json"
     @vm.run_chef
 
-    # deploy and check node application
+   # deploy and check node application
     exec_local "cd #{File.join(File.dirname(__FILE__), "..", "nodejs_app_test")} && TARGET=#{@vm.ip} cap deploy"
 
     @http.get 12345, "/"
@@ -29,6 +30,22 @@ class TestConf6 < Test::Unit::TestCase
       @http.assert_last_response_code 200
       @http.assert_last_response_body_regex /#{hostname}/
     end
+
+     # check statsd is connected to graphite
+    wait "Waiting data from statsd", 300, 10 do
+      @http.get 80, "/render/?target=statsd.numStats&rawData=csv"
+      @http.assert_last_response_code 200
+      @http.assert_last_response_body_regex /statsd.numStats/
+    end
+
+    # send data to statsd and check it's arrive in graphite
+    UDPSocket.new.send("toto:1|c", 0, @vm.ip, 8125)
+    wait "Waiting data sent to statsd appears in graphite", 300, 10 do
+      @http.get 80, "/render/?target=stats.toto&rawData=csv"
+      @http.assert_last_response_code 200
+      @http.assert_last_response_body_regex /stats.toto/
+    end
+
   end
 
 end
