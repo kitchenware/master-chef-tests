df610e33f8e348799ad54e37d5828b95d7e4b1a3
Bertrand Paquet <bpaquet@octo.com>
2012-08-31 22:28:34 +0200

    Test kibana / logstash / search

--- tests/conf3_test.rb
+++ tests/conf3_test.rb
@@ -1,17 +1,38 @@
 require File.join(File.dirname(__FILE__), '..', 'helper.rb')
+require 'json'
 
 class TestConf3 < Test::Unit::TestCase
 
   include VmTestHelper
+  include WaitHelper
 
   def test_conf3
     @vm.upload_json "conf3.json"
     @vm.run_chef
+
+    # check redmine
     @http.get 80, "/redmine"
     @http.assert_last_response_code 200
     @http.assert_last_response_body_regex /Redmine/
     date = @vm.capture("date -u && date").split("\n").map{|x| x =~ /^.*(\d\d:\d\d:\d\d).*$/; $1}
     assert_not_equal date[0], date[1]
+
+    # check kibana
+    @http.get 80, '/kibana/loader2.php?page=3'
+    @http.assert_last_response_code 200
+    json = JSON.parse @http.response.body
+    total = json['total']
+
+    @vm.run "\"echo 'abcd' >> /tmp/toto.log\""
+
+    wait "Waiting data in kibana", 15, 1 do
+      @http.get 80, '/kibana/loader2.php?page=3'
+      @http.assert_last_response_code 200
+      json = JSON.parse @http.response.body
+
+      assert_equal json['total'], total + 1
+    end
+
   end
 
 end
