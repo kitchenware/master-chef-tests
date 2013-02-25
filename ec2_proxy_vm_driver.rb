0cd11b454589277f0de2d7f6e951b1d6850c86f5
Bertrand Paquet <bpaquet@octo.com>
2013-02-25 17:43:08 +0100

    Fix driver

--- ec2_proxy_vm_driver.rb
+++ ec2_proxy_vm_driver.rb
@@ -5,7 +5,7 @@ class Ec2ProxyVmDriver < Ec2VmDriver
 
   def init
     super
-    scp File.join(File.dirname(__FILE__), 'proxy_only.sh'), "."
+    upload_file File.join(File.dirname(__FILE__), 'proxy_only.sh'), "."
     run "chmod +x proxy_only.sh && sudo ./proxy_only.sh #{ENV['PROXY_IP']} 3128"
   end
 
