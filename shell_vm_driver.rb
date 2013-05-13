1b7ed78b0ad501dbd47354a191d2066c6d532b4d
Bertrand Paquet <bpaquet@octo.com>
2013-05-13 15:55:55 +0200

    Improve shell driver

--- shell_vm_driver.rb
+++ shell_vm_driver.rb
@@ -11,8 +11,10 @@ class ShellVmDriver < VmDriver
     @name = "#{@vm_name}".gsub(/#UID#/, Time.now.to_i.to_s)
     puts "Creating vm #{@name}"
     cmd = @clone.gsub(/#NAME#/, @name)
-    result = capture_local cmd
+    result = %x{cmd}
+    code = $?.exitstatus
     puts result
+    raise "Command failed" unless code == 0
     parsed = result.match(/^IP (.*)$/)
     raise "Unable to parse command result" if !parsed
     @ip = parsed[1]
