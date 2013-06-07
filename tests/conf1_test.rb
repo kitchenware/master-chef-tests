f57849cb2816a878429ffe5d8336faff336488d3
mikrob <mrobert@octo.com>
2013-06-07 16:19:19 +0200

    upgrade test1 to test logrotate

--- tests/conf1_test.rb
+++ tests/conf1_test.rb
@@ -3,6 +3,7 @@ require File.join(File.dirname(__FILE__), '..', 'helper.rb')
 class TestConf1 < Test::Unit::TestCase
 
   include VmTestHelper
+  include WaitHelper
 
   def test_conf1
     @vm.upload_json "conf1.json"
@@ -14,6 +15,18 @@ class TestConf1 < Test::Unit::TestCase
     @http.assert_last_response_body_regex /Confluence Setup Wizard/
     @vm.run "sudo netstat -nltp | grep 127.0.0.1:9999 | grep LISTEN | grep java"
     @vm.run "sudo netstat -nltp | grep 127.0.0.1:3306 | grep LISTEN"
+
+    @vm.run "echo 'pouet\npipo\nmolo\nbidule\nchose\n' > /home/chef/fake.log"
+    @vm.run "sudo logrotate -f /etc/logrotate.d/fake"
+    rotated_file = @vm.capture("ls /home/chef/fake.log.1")
+    assert_equal "/home/chef/fake.log.1\n", rotated_file
+
+    @vm.run "sudo touch /etc/logrotate.d/todelete"
+    @vm.run_chef
+
+    files = @vm.capture "ls -1 /etc/logrotate.d/"
+    assert_false files.split("\n").include?("todelete")
+
   end
 
 end
